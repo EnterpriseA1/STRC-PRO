854e01fcd6f73d4a1939ea8542cc72ae227c299b
Kritsakorn Khamwiset <95012380+KKhamwiset@users.noreply.github.com>
2024-01-13 03:08:01 +0700

    Add files via upload

--- SRSLab4/SRSLab4Q6.c
+++ SRSLab4/SRSLab4Q6.c
@@ -3,4 +3,11 @@
 int main(){
     char ISBN[11] = {'\0'};
     scanf("%s",ISBN);
+    int result = 0;
+    for (int i = 0 ; i < 11 ; i++){
+        result += (ISBN[i] - '0') *  (11 - (i + 1));
+    }
+    int valid = (result % 11) == 0;
+    printf("%s",(valid) ? "valid":"invalid");
+
 }

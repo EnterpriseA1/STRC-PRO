1633a1832bd43583656d9666bf6510ac21db5840
premnacup <128221757+premnacup@users.noreply.github.com>
2024-03-14 16:25:30 +0700

    Update test2.c

--- grader/pre-quiz-struct/test2.c
+++ grader/pre-quiz-struct/test2.c
@@ -1,37 +1,44 @@
-#include<stdio.h>
-typedef struct {
+#include <stdio.h>
+typedef struct
+{
 	char name[16];
 	char surname[20];
 	int score[5];
 	float stotal;
 } Student;
-void readStudentData(Student *,int);
-void printScore2(Student[],int);
+void readStudentData(Student *, int);
+void printScore2(Student[], int);
 
-void main(){
+void main()
+{
 	int n;
-	scanf("%d",&n);
-	
+	scanf("%d", &n);
+
 	Student std[n];
-	
+
 	int i;
-	for(i = 0;i<n;i++){
-		readStudentData(&std[i],n);
+	for (i = 0; i < n; i++)
+	{
+		readStudentData(&std[i], n);
 	}
-	printScore2(&std,n);
+	printScore2(&std, n);
 }
 
-void readStudentData(Student * std,int n){
-	scanf("%s %s", std->name,std->surname);
+void readStudentData(Student *std, int n)
+{
+	scanf("%s %s", std->name, std->surname);
 	int i;
-	for(i=0;i<n;i++){
+	for (i = 0; i < n; i++)
+	{
 		scanf("%d", &std->score[i]);
 	}
 }
 
-void printScore2(Student std[],int n){
+void printScore2(Student std[], int n)
+{
 	int i;
-	for(i=0;i<n;i++){
+	for (i = 0; i < n; i++)
+	{
 		printf("%s %d\n", std[i].name, std[i].score[1]);
 	}
 }

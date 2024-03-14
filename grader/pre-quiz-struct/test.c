c36c724ba24912aefb49f6f8584d7671861bc34c
premnacup <128221757+premnacup@users.noreply.github.com>
2024-03-14 17:46:44 +0700

    Update test.c

--- grader/pre-quiz-struct/test.c
+++ grader/pre-quiz-struct/test.c
@@ -1,43 +1,56 @@
-#include<stdio.h>
-#include<string.h>
-typedef struct {
+#include <stdio.h>
+#include <string.h>
+typedef struct
+{
 	char name[64];
 	char phoneNumber[32];
 } phoneBook;
 
 void addRecord(char *someone, char *pnumber);
-char *searchPhone(char *name, phoneBook);
+char *searchPhone(char *name, phoneBook *dir);
 
-void main(){
+void main()
+{
 	phoneBook directory[50];
 	int n;
-	scanf("%d",&n);
-	
+	scanf("%d", &n);
+
 	int i;
-	for(i = 0;i<n;i++){
-		addRecord(&directory[i].name,&directory[i].phoneNumber);
+	for (i = 0; i < n; i++)
+	{
+		addRecord(&directory[i].name, &directory[i].phoneNumber);
 	}
-	
+
 	int searchcount = 0;
-	scanf("%d",&searchcount);
-	char *name;
-	for(i = 0;i<searchcount;i++){
+	scanf("%d", &searchcount);
+	char name[64];
+	for (i = 0; i < searchcount; i++)
+	{
 		scanf("%s", name);
-		searchPhone(name, &directory[]);
+		printf("%s\n", searchPhone(&name, &directory[i]));
 	}
 }
 
-void addRecord(char *someone, char *pnumber){
-	scanf("%s %s", someone,pnumber);
+void addRecord(char *someone, char *pnumber)
+{
+	scanf("%s %s", someone, pnumber);
 }
 
-char *searchPhone(char *name,phoneBook *dir){
-	char * result;
-	if(!(strcmp(name,dir->name))){
-		result = dir->name;
-	}
-	else{
-		result = "N/A";
+char *searchPhone(char *name, phoneBook *dir)
+{
+	char *result;
+	int i;
+	for (i = 0; i < 50; i++)
+	{
+		if (strcmp(dir[i].name, name) == 0)
+		{
+			result = dir[i].phoneNumber;
+			break;
+		}
+		else
+		{
+			result = "N/A";
+		}
 	}
 	return result;
 }

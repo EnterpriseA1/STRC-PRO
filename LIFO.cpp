c12c5ae4d94438ce016fb2602f4774190784903d
Kritsakorn Khamwiset <95012380+KKhamwiset@users.noreply.github.com>
2024-08-08 17:20:23 +0700

    Update LIFO.cpp

--- LIFO.cpp
+++ LIFO.cpp
@@ -3,105 +3,95 @@
 using namespace std;
 
 class Node{
-	public:
-		int id;
-		int arrivalTime;
-		int serviceTime;
-		bool gotInsert;
-		Node (int id, int at,int st){
-			this->id = id;
-			this->arrivalTime = at;
-			this->serviceTime = st;
-			this->gotInsert = false;
-	}
+    public:
+        int id;
+        int arrivalTime;
+        int serviceTime;
+        bool gotInsert;
+        Node(){
+            gotInsert = false;
+        }
+        Node (int id, int at,int st){
+            this->id = id;
+            this->arrivalTime = at;
+            this->serviceTime = st;
+            this->gotInsert = false;
+    }
 };
 class stack{
-	public:
-		vector<Node> Stack;
-		int top = -1;
-		void push(Node newNode){
-			top++;
-			if (top >= 10000){
-				cout<<"Stack is full"<<endl;
-				return;
-			}
-			
-			Stack.push_back(newNode);
-		}
-		void pop(){
-			if (top <= -1){
-				cout<<"Stack is empty"<<endl;
-				return;
-			}
-			top--;
-		}
-		bool availableNode(int time, vector<Node>arr){
-			bool isFound = false;
-			for (int i = 0 ; i <= top ;i++){
-				if (arr[i].serviceTime == time){
-					isFound = true;
-					Stack.push_back(arr[i]);
-					return isFound;
-				}
-			}
-			return isFound;
-		}
-		void process(int* processTimer,vector<Node>arr){
-			while (availableNode(*processTimer,arr)){
-				if (Stack[top].serviceTime <= 0){
-					pop();
-				}
-				if (Stack[top].serviceTime > 0){
-					cout<<Stack[top].id<<" : "<<Stack[top].serviceTime<<endl;
-					Stack[top].serviceTime--;
-				}
-				(*processTimer)++;
-			}
-		}
-		
-		void printAll(){
-			cout<<top<<endl;
-			for (int i = top; i >= 0 ;i--){
-				cout<<Stack[i].id<<" "<<Stack[i].arrivalTime<<" "<<Stack[i].serviceTime<<endl;
-			}
-		}
+    public:
+        Node *Stack = new Node[10000];
+        int top = -1;
+        void push(Node newNode){
+            top++;
+            if (top >= 10000){
+                cout<<"Stack is full"<<endl;
+                return;
+            }
+            
+            Stack[top] = newNode;
+        }
+        void pop(){
+            if (top <= -1){
+                cout<<"Stack is empty"<<endl;
+                return;
+            }
+            top--;
+        }
+        void insertNode(int time, Node* arr,int n){
+            for (int i = 0 ; i < n ;i++){
+                if (arr[i].arrivalTime == time){
+                    arr[i].gotInsert = true;
+                    push(arr[i]);
+                }
+            }
+            return;
+        }
+        bool allServiceEmpty(){
+            for (int i = 0 ; i <= top ;i++){
+                if (Stack[i].serviceTime > 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+        void process(int* processTimer,Node* arr,int n){
+            int currentProcess = top;
+            while (!allServiceEmpty()){
+                if (Stack[currentProcess].serviceTime <= 0){
+                    pop();
+                    insertNode(*(processTimer),arr,n);
+                    currentProcess = top;
+                }
+                if (Stack[currentProcess].serviceTime > 0){
+                    cout<<Stack[currentProcess].id<<" : "<<Stack[currentProcess].serviceTime<<endl;
+                    Stack[currentProcess].serviceTime--;
+                }
+                insertNode(++(*processTimer),arr,n);
+            }
+        }
+        
+        void printAll(){
+            cout<<top<<endl;
+            for (int i = top; i > -1 ;i--){
+                cout<<Stack[i].id<<"-"<<Stack[i].arrivalTime<<"-"<<Stack[i].serviceTime<<endl;
+            }
+        }
 };
 
-void swap(Node* one,Node *two){
-	Node temp = *one;
-	*one = *two;
-	*two = temp;
-}
+
 int main(){
-	int n,id,at,st;
-	cin>>n;
-	int minAt = INT_MAX;
-	int processTimer = 0;
-	stack newStack;
-	vector<Node>arr;
-	for (int i = 0 ; i < n ;i++){
-		cin>>id>>at>>st;
-		Node process = Node(id,at,st);
-		arr.push_back(process);
-	}
-
-	newStack.printAll();
-	cout<<endl;
-	newStack.process(&processTimer,arr);
-
-	;}
-/*
-4
-1010
-2
-3
-2002
-0
-4
-3030
-4
-3
-4040
-3
-2*/
-
+    int n,id,at,st;
+    cin>>n;
+    int processTimer = 0;
+    stack newStack;
+    Node *arr = new Node[n];
+    for (int i = 0 ; i < n ;i++){
+        cin>>id>>at>>st;
+        Node process = Node(id,at,st);
+        arr[i] = process;
+    }
+    cout<<endl;
+         newStack.insertNode(processTimer,arr,n);
+    newStack.process(&processTimer,arr,n);
+    ;}

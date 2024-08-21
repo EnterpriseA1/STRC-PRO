f5dd38a6a44ae56c436284b2a2a45f6bb4059941
Kritsakorn Khamwiset <95012380+KKhamwiset@users.noreply.github.com>
2024-08-22 00:03:04 +0700

    Add files via upload

---
+++ LAB6NO5.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab6;
+
+/**
+ *
+ * @author KKhamvised
+ */
+import java.util.Date;
+
+class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+    public Account() {
+        this.id = 0;
+        this.balance = 0.0;
+        this.annualInterestRate = 0.0;
+        this.dateCreated = new Date();
+    }
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = 0.0;
+        this.dateCreated = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public double getMonthlyInterestRate() {
+        return annualInterestRate / 12;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * getMonthlyInterestRate() / 100;
+    }
+    public void withdraw(double amount) {
+       this.balance -= amount;
+    }
+    public void deposit(double amount) {
+       this.balance += amount;
+    }
+}
+public class LAB6NO5 {
+    public static void main(String[] args) {
+        Account account = new Account(1122, 20000);
+        account.setAnnualInterestRate(4.5);
+        account.withdraw(2500);
+        account.deposit(3000);
+        System.out.println("Account ID: " + account.getId());
+        System.out.println("Balance: " + account.getBalance());
+        System.out.println("Monthly Interest Rate: " + account.getMonthlyInterestRate() + "%");
+        System.out.println("Monthly Interest: " + account.getMonthlyInterest());
+    }
+}

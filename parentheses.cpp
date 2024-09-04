bb84706a52c327afa66412a6f0fcde5ad90bbc67
Kritsakorn Khamwiset <95012380+KKhamwiset@users.noreply.github.com>
2024-09-05 03:14:53 +0700

    Update parentheses.cpp

--- parentheses.cpp
+++ parentheses.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isMatching(char a, char b){
-    return (a == '(' && b == ')') ||
-           (a == '{' && b == '}') ||
-           (a == '[' && b == ']') ||
-           (a == '<' && b == '>');
+bool isMatching(string a, string b){
+    return (a == "(" && b == ")") ||
+           (a == "{" && b == "}") ||
+           (a == "[" && b == "]") ||
+           (a == "<" && b == ">") ||
+           (a == "<<" && b == ">>");
 }
 
 int main() {
@@ -14,29 +15,34 @@ int main() {
     getline(cin,input);
     int match = 0;
     bool isClosingBracketFrontal = false;
-    for (int i = 0 ; i < input.size() - 1 && !isClosingBracketFrontal ;i++){
-        if (input[i] == '<' && input[i+1] == '<'){
-            str.push("<<");
-            i++;
-        }
-        else if (input[i] == '>' && input[i+1] == '>'){
-            if (!str.empty() && str.top() == "<<"){
-                str.pop();
-                match++;
+    for (int i = 0 ; i < input.size() && !isClosingBracketFrontal ;i++){
+        bool foundDouble = false;
+        if (i < input.size() - 1){
+            if (input[i] == '<' && input[i+1] == '<'){
+                str.push("<<");
                 i++;
+                foundDouble = true;
             }
-            else{
-                if (!str.empty()){
+            if (input[i] == '>' && input[i+1] == '>'){
+                if (!str.empty() && str.top() == "<<"){
                     str.pop();
+                    match++;
+                    i++;
+                    foundDouble = true;
                 }
+                else{
+                    if (!str.empty()){
+                        str.pop();
+                    }
+                }
+                
             }
-            
         }
-        else if (input[i] == '(' || input[i] == '[' || input[i] ==  '{' || input[i] == '<' ){
+        if (input[i] == '(' || input[i] == '[' || input[i] ==  '{' || input[i] == '<'  && !foundDouble){
             str.push(string(1,input[i]));
         }
-        else if (input[i] == ')' || input[i] == ']' || input[i] ==  '}' || input[i] == '>' ){
-            if (!str.empty() && isMatching(str.top()[0],input[i])){
+        if (input[i] == ')' || input[i] == ']' || input[i] ==  '}' || input[i] == '>' && !foundDouble){
+            if (!str.empty() && isMatching(str.top(),string(1,input[i]))){
                 str.pop();
                 match++;
             }
@@ -49,6 +55,8 @@ int main() {
                 }
             }
         }
+
+       
     }
     if (str.empty() && !isClosingBracketFrontal){
         cout << match << " matched" << endl;
